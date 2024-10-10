9d9b5b4e8a20b295145f761b1fe9c3245c46e55e
Rikhard <rikhard.badorek@student.lut.fi>
2024-10-10 16:39:52 +0300

    FAQ added

---
+++ Course_Work/js/main.js
@@ -0,0 +1,20 @@
+// FAQ
+
+document.addEventListener('DOMContentLoaded', () => {
+    const faqContainer = document.querySelector('.faq-content')
+
+    faqContainer.addEventListener('click', (e) => {
+        const groupHeader = e.target.closest(".faq-group-header")
+
+        if (!groupHeader) return;
+
+        const group = groupHeader.parentElement
+        const groupBody = group.querySelector(".faq-group-body")
+        const icon = groupHeader.querySelector("i")
+
+        icon.classList.toggle("fa-plus")
+        icon.classList.toggle("fa-minus")
+
+        groupBody.classList.toggle("open")
+    })
+})
