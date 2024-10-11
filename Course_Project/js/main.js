026092e0be9e766a79f906842253a00a796df8be
Rikhard <rikhard.badorek@student.lut.fi>
2024-10-11 14:10:06 +0300

    Project layout and styling finished

--- Course_Project/js/main.js
+++ Course_Project/js/main.js
@@ -7,3 +7,22 @@ document.addEventListener('DOMContentLoaded', () => {
         hamburgerButton.classList.toggle("menu-open");
     });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const faqContainer = document.querySelector('.faq-content')
+
+    faqContainer.addEventListener('click', (e) => {
+        const groupHeader = e.target.closest(".faq-group-header")
+
+        if (!groupHeader) return;
+
+        const group = groupHeader.parentElement
+        const groupBody = group.querySelector(".faq-group-body")
+        const icon = groupHeader.querySelector("i")
+
+        icon.classList.toggle("fa-plus")
+        icon.classList.toggle("fa-minus")
+
+        groupBody.classList.toggle("open")
+    })
+})
